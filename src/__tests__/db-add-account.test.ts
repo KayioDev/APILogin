7fb712e823eee586d313eddcdc5b7043219a6fdb
Kaio <kylelima850@hotmail.com>
2025-03-15 20:31:07 -0300

    refactor: encapsulando o Sut e o EncrypterStub em um factory method

--- src/__tests__/db-add-account.test.ts
+++ src/__tests__/db-add-account.test.ts
@@ -1,13 +1,27 @@
+import { Encrypter } from "../Data/Protocol/encrypter";
 import { DbAddAccount } from "../Data/Usecases/db-add-account";
+interface SutTypes {
+    sut: DbAddAccount,
+    encriptyStub: Encrypter
+}
+const makeSut = ():SutTypes => {
+    class EncriptStub {
+        async encrypt (valor: string): Promise<string> {
+            return new Promise(resolve => resolve('hashed_value'));
+        }
+    }
+    const encriptyStub = new EncriptStub();
+    const sut = new DbAddAccount(encriptyStub);
+    return {
+        sut,
+        encriptyStub
+    }
+}
+
+
 describe ('DbAddAccount Usecases', ()=> {
     test ('Quarantir que encrypter seja chamado com a senha correta', async () =>{
-        class EncriptStub {
-            async encrypt (valor: string): Promise<string> {
-                return new Promise(resolve => resolve('hashed_value'));
-            }
-        }
-        const encriptyStub = new EncriptStub();
-        const sut = new DbAddAccount(encriptyStub);
+        const {encriptyStub, sut} = makeSut()
         const encriptSpy = jest.spyOn(encriptyStub, 'encrypt')
         const accountData = {
             nome:'nome_valido',
